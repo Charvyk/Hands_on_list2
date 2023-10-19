f80cf76387cdbb48672c266013ac959d532a3b82
Charvyk <67979782+Charvyk@users.noreply.github.com>
2023-10-19 11:18:41 +0530

    Add files via upload

---
+++ 014.c
@@ -0,0 +1,36 @@
+/*
+============================================================================
+Name : 14.c
+Author :charvy koshta
+Description : Write a simple program to create a pipe, write to the pipe, read from pipe and display on
+the monitor.
+Date: 30th sept, 2023.
+============================================================================
+*/
+
+
+
+#include<stdio.h>
+#include<unistd.h>
+
+int main(){
+  int fd[2];
+  int status;
+  char buff[35];
+  int r, w;
+  if((status=pipe(fd)) == -1) printf("\nError creating pipe!!");
+  else{
+	//writing to the pipe
+	w = write(fd[1], "Hello World written to the pipe!!",33);
+	if(w==-1) printf("\n Unable to write to the pipe");
+	else
+	{
+		//reading for pipe
+		printf("\n Reading...");
+		r = read(fd[0], &buff, w);
+		if(r==-1) printf("\n Unable to read!!");
+		else printf("\nThe pipe read : %s", buff);
+	} 
+  }
+  return 0;
+}
